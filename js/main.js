003337ccc8dfb439466ec59b05a7ffd0975dde3a
smcmin <mmm3n5@gmail.com>
2024-06-16 17:14:53 +0900

    제이쿼리

--- js/main.js
+++ js/main.js
@@ -3,3 +3,33 @@ document.querySelector("h1").addEventListener("click", function () {
     alert("안녕하세요!");
   }
 });
+
+//  header hover
+// document.querySelector(".header-product").addEventListener("mouseover", function(){
+//   {
+//     console.log('허버됨');
+//     document.querySelector(".depth-2").style.display = "block"
+//   }
+// })
+
+
+$(document).ready(function () {
+  $(".depth-1-item").hover(
+    function () {
+      $(this).children().next().addClass('hover');
+    },
+    function () {
+      $(this).children().next().removeClass('hover');
+    }
+  );
+
+  // $('.swiper-slide.swiper-slide-visible').children().next().addClass('fadeup-ani');
+
+  // console.log($('.swiper-slide.swiper-slide-visible').children().next())
+
+ 
+}
+
+
+);
+
